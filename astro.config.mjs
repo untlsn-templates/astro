17382f270fed54e10d5c424575f3c26b38aacf6b
Filip Skoczen <philipskoczen@gmail.com>
2023-02-04 14:49:17 +0100

    Create astro template

---
+++ astro.config.mjs
@@ -0,0 +1,9 @@
+import { defineConfig } from 'astro/config';
+import unocss from 'unocss/vite';
+
+// https://astro.build/config
+export default defineConfig({
+  vite: {
+    plugins: [unocss()],
+  }
+});
