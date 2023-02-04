2d8be3a8fa8c61e8bff595d04610ca07c35c0714
Filip Skoczen <philipskoczen@gmail.com>
2023-02-04 14:52:56 +0100

    Add minify

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,12 @@ import { defineConfig } from 'astro/config';
 import unocss from 'unocss/vite';
 
 // https://astro.build/config
+import frontendistahtmlMinify from "@frontendista/astro-html-minify";
+
+// https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [unocss()],
-  }
+    plugins: [unocss()]
+  },
+  integrations: [frontendistahtmlMinify()]
 });
